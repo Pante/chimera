da6bbb5bc90fa5c965b3cb7ddd2c0c10c7a2c6f8
Pante <matthiasngeozhongsim@yahoo.com>
2016-09-22 23:38:00 +0800

    Initial commit

---
+++ src/main/java/com/karusmc/xmc/core/Default.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016 PanteLegacy @ karusmc.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.karusmc.xmc.core;
+
+/**
+ *
+ * @author PanteLegacy @ karusmc.com
+ */
+public enum Default {
+    
+    NONE("none"), OP("op"), ALL("all");
+    
+    
+    public final String name;
+    
+    private Default(String name) {
+        this.name = name;
+    }
+    
+    
+    public static Default fromString(String name) {
+        for (Default value: Default.values()) {
+            if (value.name.equalsIgnoreCase(name)) {
+                return value;
+            }
+        }
+        
+        throw new IllegalArgumentException("No enum with name: " + name + " was found");
+    }
+
+}
