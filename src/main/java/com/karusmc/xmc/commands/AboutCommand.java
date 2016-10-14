e617a2a366f226edae89bb8fae88cba60c93baee
Pante <matthiasngeozhongsim@yahoo.com>
2016-10-14 23:34:09 +0800

    Added Default commands

---
+++ src/main/java/com/karusmc/xmc/commands/AboutCommand.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 PanteLegacy @ karusmc.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.karusmc.xmc.commands;
+
+import com.karusmc.xmc.core.*;
+
+import org.bukkit.*;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.*;
+
+import static com.karusmc.xmc.util.Validator.*;
+
+/**
+ *
+ * @author PanteLegacy @ karusmc.com
+ */
+public class AboutCommand extends XMCommand {
+    
+    private String pluginDescription;
+    
+    
+    public AboutCommand(Plugin plugin, String name) {
+        super(plugin, name);
+        
+        PluginDescriptionFile description = plugin.getDescription();
+        pluginDescription = ChatColor.translateAlternateColorCodes('&', 
+                "&6" + description.getName() 
+                + "version: &c" + description.getVersion() 
+                + "\n&6" + description.getDescription()
+                + "\nAuthor(s): &c" + description.getAuthors().toString() 
+                + "\n&6Source code & development resources: &c" + description.getWebsite());
+    }
+    
+    
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (canUse(this, sender) && hasLength(0, args.length, 0)) {
+            sender.sendMessage(pluginDescription);
+            
+        } else {
+            sender.sendMessage(ChatColor.RED + getPermissionMessage());
+        }
+        
+        return true;
+    }
+    
+}
---
+++ src/main/java/com/karusmc/xmc/xml/ParserRegister.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 PanteLegacy @ karusmc.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.karusmc.xmc.xml;
+
+import com.karusmc.xmc.core.*;
+
+import java.util.*;
+
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author PanteLegacy @ karusmc.com
+ */
+public class ParserRegister extends XMCommand implements Dispatcher {
+    
+    private Map<String, XMCommand> commands;
+    
+    
+    public ParserRegister() {
+        super(null, null);
+        commands = new HashMap<>();
+    }
+
+    
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        throw new UnsupportedOperationException();
+    }
+    
+
+    @Override
+    public Map<String, XMCommand> getCommands() {
+        return commands;
+    }
+    
+}
