e3784a324c4a00055b312edca8e61f2da034d535
Pante <matthiasngeozhongsim@yahoo.com>
2018-06-03 23:21:45 +0800

    Redid types

--- annotations/src/main/java/com/karuslabs/annotations/signature/Type.java
+++ annotations/src/main/java/com/karuslabs/annotations/signature/Type.java
@@ -23,12 +23,64 @@
  */
 package com.karuslabs.annotations.signature;
 
-import com.sun.source.tree.Tree;
+import com.sun.source.tree.*;
 
 import java.util.function.Predicate;
+import javax.lang.model.type.TypeKind;
+
+import static javax.lang.model.type.TypeKind.*;
 
 
 @FunctionalInterface
 public interface Type<T extends Tree> extends Predicate<T> {
+    
+    public static Type<PrimitiveTypeTree> primitive(Class<?> type) {
+        TypeKind expected = map(type);
+        return tree -> tree.getPrimitiveTypeKind() == expected;
+    }
+    
+    public static Type<IdentifierTree> of(Class<?> type) {
+        String name = type.getName();
+        return tree -> tree.getName().contentEquals(name);
+    }
+    
+    public static Type<IdentifierTree> from() {
+        
+    }
+            
+    public static Type<IdentifierTree> to() {
+        
+    }
+    
+    
+    public static TypeKind map(Class<?> primitive) {
+        if (primitive == boolean.class) {
+            return BOOLEAN;
+            
+        } else if (primitive == byte.class) {
+            return BYTE;
+            
+        } else if (primitive == short.class) {
+            return SHORT;
+            
+        } else if (primitive == int.class) {
+            return INT;
+            
+        } else if (primitive == long.class) {
+            return LONG;
+            
+        } else if (primitive == char.class) {
+            return CHAR;
+            
+        } else if (primitive == float.class) {
+            return FLOAT;
+            
+        } else if (primitive == double.class) {
+            return DOUBLE;
+            
+        } else {
+            throw new IllegalArgumentException("Invalid class, class must be a primitive");
+        }
+    }
     
 }
