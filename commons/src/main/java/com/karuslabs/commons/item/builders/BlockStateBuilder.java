5b2e4384a886f4ff6b0681102d73044a9aad1ed6
Pante <matthiasngeozhongsim@yahoo.com>
2018-08-09 23:17:37 +0800

    Reorganised item package
    Preliminary investigation into the command framework has revealed that
    the only blocker is registering the commands to the CommandDispatcher
    instance as Spigot replaces it every single time plugins are
    enabled/reloaded. This proves to be problematic as there are no proper
    events to detect, It is imperative that the ServerLoadEvent PR is
    accepted

---
+++ commons/src/main/java/com/karuslabs/commons/item/builders/BlockStateBuilder.java
@@ -0,0 +1,57 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Karus Labs.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.karuslabs.commons.item.builders;
+
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.inventory.meta.*;
+
+
+public class BlockStateBuilder extends Builder<BlockStateMeta, BlockStateBuilder> {
+    
+    public static BlockStateBuilder of(Material material) {
+        return new BlockStateBuilder(material);
+    } 
+    
+    BlockStateBuilder(Material material) {
+        super(material);
+    }
+    
+    BlockStateBuilder(Builder<ItemMeta, ?> source) {
+        super(source);
+    }
+    
+    
+    public BlockStateBuilder state(BlockState state) {
+        meta.setBlockState(state);
+        return this;
+    }
+    
+
+    @Override
+    protected BlockStateBuilder self() {
+        return this;
+    }
+    
+}
---
+++ commons/src/main/java/com/karuslabs/commons/item/builders/ItemBuilder.java
@@ -0,0 +1,95 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Karus Labs.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.karuslabs.commons.item.builders;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.meta.ItemMeta;
+
+
+public class ItemBuilder extends Builder<ItemMeta, ItemBuilder> {
+    
+    public static ItemBuilder of(Material material) {
+        return new ItemBuilder(material);
+    }
+    
+    ItemBuilder(Material material) {
+        super(material);
+    }
+    
+    
+    public BannerBuilder asBanner() {
+        return new BannerBuilder(this);
+    }
+    
+    public BlockStateBuilder asBlockState() {
+        return new BlockStateBuilder(this);
+    }
+    
+    public BookBuilder asBook() {
+        return new BookBuilder(this);
+    }
+    
+    public EnchantmentStorageBuilder asEnchantmentStorage() {
+        return new EnchantmentStorageBuilder(this);
+    }
+    
+    public FireworkBuilder asFirework() {
+        return new FireworkBuilder(this);
+    }
+    
+    public FireworkEffectBuilder asFireworkEffect() {
+        return new FireworkEffectBuilder(this);
+    }
+    
+    public KnowledgeBookBuilder asKnowledgeBook() {
+        return new KnowledgeBookBuilder(this);
+    }
+    
+    public LeatherArmourBuilder asLeatherArmour() {
+        return new LeatherArmourBuilder(this);
+    }
+    
+    public MapBuilder asMap() {
+        return new MapBuilder(this);
+    }
+    
+    public PotionBuilder asPotion() {
+        return new PotionBuilder(this);
+    }
+    
+    public SkullBuilder asSkull() {
+        return new SkullBuilder(this);
+    }
+    
+    public TropicalFishBucketBuilder asBucket() {
+        return new TropicalFishBucketBuilder(this);
+    }
+
+    
+    @Override
+    protected ItemBuilder self() {
+        return this;
+    }
+    
+}
